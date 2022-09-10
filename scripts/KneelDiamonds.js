e373470b9d36f677b93a755d1646f39270bcabb8
Ganesh Babu <ganesh.b@live.com>
2022-09-10 13:25:56 -0400

    added event listeners and display order

--- scripts/KneelDiamonds.js
+++ scripts/KneelDiamonds.js
@@ -1,27 +1,31 @@
+import { DiamondSizes } from "./DiamondSizes.js";
+import { JewelryStyles } from "./JewelryStyles.js";
+import { Orders } from "./Orders.js";
+import { Metals } from "./Metals.js";
+import { addCustomOrder } from "./database.js";
 
-import { DiamondSizes } from "./DiamondSizes.js"
-import { JewelryStyles } from "./JewelryStyles.js"
-import { Orders } from "./Orders.js"
-
-document.addEventListener(
-    "click",
-    (event) => {
-    }
-)
+document.addEventListener("click", (event) => {
+  if (event.target.id === "orderButton") {
+    addCustomOrder();
+  }
+});
 
 export const KneelDiamonds = () => {
-    return `
+  return `
         <h1>Kneel Diamonds</h1>
 
         <article class="choices">
             <section class="choices__metals options">
                 <h2>Metals</h2>
+                ${Metals()}
             </section>
             <section class="choices__sizes options">
                 <h2>Sizes</h2>
+                ${DiamondSizes()}
             </section>
             <section class="choices__styles options">
                 <h2>Styles</h2>
+                ${JewelryStyles()}
             </section>
         </article>
 
@@ -31,7 +35,7 @@ export const KneelDiamonds = () => {
 
         <article class="customOrders">
             <h2>Custom Jewelry Orders</h2>
+            ${Orders()}
         </article>
-    `
-}
-
+    `;
+};
--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,25 +1,40 @@
-import { getOrders } from "./database.js"
+import { getMetals, getOrders } from "./database.js";
 
 const buildOrderListItem = (order) => {
-    return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
-    </li>`
-}
+  const metals = getMetals();
+
+  const foundMetal = metals.find((metal) => {
+    return metal.id === order.metalId;
+  });
+
+  const totalcost = foundMetal.price;
+  const costString = totalcost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const formattedTimeStamp = new Date(order.timestamp).toLocaleDateString(
+    "en-US",
+    { weekday: "long", year: "numeric", month: "long", day: "numeric" }
+  );
+  return `<li>
+        Order #${order.id} was placed on ${formattedTimeStamp} Total Cost: ${costString}
+    </li>`;
+};
 
 export const Orders = () => {
-    /*
+  /*
         Can you explain why the state variable has to be inside
         the component function for Orders, but not the others?
     */
-    const orders = getOrders()
+  const orders = getOrders();
 
-    let html = "<ul>"
+  let html = "<ul>";
 
-    const listItems = orders.map(buildOrderListItem)
+  const listItems = orders.map(buildOrderListItem);
 
-    html += listItems.join("")
-    html += "</ul>"
+  html += listItems.join("");
+  html += "</ul>";
 
-    return html
-}
-
+  return html;
+};
